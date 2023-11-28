e019beffea15313e4af7136a44951370102ba9b5
betty29tesfaye <144184498+betty29tesfaye@users.noreply.github.com>
2023-11-28 09:35:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def add_background_image(image_file):
 add_background_image('bgi.png')   
 st.markdown(f'<span style="background-color:#DFF2FF;color:#0F52BA;font-family:book-antiqua;font-size:24px;">AI App For Translation</span>', unsafe_allow_html=True)
 st.markdown(f'<p style="background-color:#b3cee5;color:#414C6B;">Enter text you want to get translated</p>', unsafe_allow_html=True) 
-text_to_translate = st.text_input("Enter text you want to get translated")
+text_to_translate = st.text_input("")
 if text_to_translate:
   translator = Translator()
   lang = translator.detect(text_to_translate).lang
@@ -27,4 +27,4 @@ if text_to_translate:
     translated_txt = translator.translate(text_to_translate, src= lang, dest= 'en')
   else:
     translated_txt = translator.translate(text_to_translate, src= lang, dest='am')
-    st.markdown(f'<p style="background-color:#b3cee5;color:#414C6B;">{translated_txt.text}</p>', unsafe_allow_html=True) 
+  st.markdown(f'<p style="background-color:#b3cee5;color:#414C6B;">{translated_txt.text}</p>', unsafe_allow_html=True) 
